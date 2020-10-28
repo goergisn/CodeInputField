1b089702159a0d4aa1cba4ca4ca7f0cd85df1557
Alex Guretzki <alex@one.fit>
2020-10-28 10:17:23 +0100

    added preview assets

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CodeInputField",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["Resources/*"]),
         .testTarget(
             name: "CodeInputFieldTests",
             dependencies: ["CodeInputField"]),
